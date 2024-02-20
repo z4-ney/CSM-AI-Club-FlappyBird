15f31954e7e67901bf2b7309242401a8d9f05ef0
Pham Hoang Nam <twinsquidz@gmail.com>
2024-02-20 13:47:34 -0800

    wemakthegem

--- game/game.py
+++ game/game.py
@@ -1,1 +1,26 @@
+import pygame
 
+screen = pygame.display.set_mode((1600, 900)) 
+
+# Set the caption of the screen 
+pygame.display.set_caption('Gwyneth Paltrow')
+
+# screen is green
+colon = (0,255,0)
+screen.fill(colon)
+
+# update is game
+pygame.display.flip()
+
+# game is run
+running = True
+
+# game loop 
+while running: 
+    
+# for loop through the event queue   
+    for event in pygame.event.get(): 
+      
+        # Check for QUIT event       
+        if event.type == pygame.QUIT: 
+            running = False
