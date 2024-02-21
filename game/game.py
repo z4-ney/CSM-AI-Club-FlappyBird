ca68bb87496fa25e1ea1eeff1f95ad6795a48a50
Pham Hoang Nam <twinsquidz@gmail.com>
2024-02-20 20:13:46 -0800

    I DONT KNOW WHAT I AM DOING

--- game/game.py
+++ game/game.py
@@ -1,26 +1,41 @@
 import pygame
 
-screen = pygame.display.set_mode((1600, 900)) 
+# pygame innit
+pygame.init()
+clock = pygame.time.Clock()
+
+# the screen is this big
+screen = pygame.display.set_mode((450, 800)) 
+colon = (0,255,0)
+screen.fill(colon)
 
 # Set the caption of the screen 
 pygame.display.set_caption('Gwyneth Paltrow')
 
-# screen is green
-colon = (0,255,0)
-screen.fill(colon)
+# Gayme
+scroll_speed = 1
 
-# update is game
-pygame.display.flip()
+# class Ground():
+     # def __init__(self,x,y):
+          
 
-# game is run
-running = True
-
-# game loop 
-while running: 
-    
-# for loop through the event queue   
+def quit_game():
     for event in pygame.event.get(): 
-      
-        # Check for QUIT event       
-        if event.type == pygame.QUIT: 
-            running = False
+        
+            # Check for QUIT event       
+            if event.type == pygame.QUIT: 
+                pygame.quit()
+                exit()
+
+def main():
+    # game is run
+    run = True
+
+    # game loop 
+    while run: 
+        quit_game()
+        screen.fill(colon)
+        clock.tick(60)
+        pygame.display.update()
+
+main()
